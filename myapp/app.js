6ab5300970bad8cb9d87213eed58623c66556fc8
Parth <Parthpurbia@gmail.com>
2025-08-16 14:59:17 +0530

    modification

--- myapp/app.js
+++ myapp/app.js
@@ -5,4 +5,4 @@ const port = 3000
 
 app.get('/', (_, res) => res.sendFile(path.join(__dirname, 'index.html')))
 
-app.listen(port, () => console.log(`Listening on port ${port}...`))
+app.listen(port, '0.0.0.0', () => console.log(`Listening on port ${port}...`))
